d0dcec95aeab10dffa646d718942465d8096e72a
Imene Khemiri <khemiriimene@gmail.com>
2018-12-11 14:11:07 +0100

    Update the test scenarios

--- FooBarQixToolkit.Tests/FooBarQixToolkitTests.cs
+++ FooBarQixToolkit.Tests/FooBarQixToolkitTests.cs
@@ -18,58 +18,91 @@ namespace FooBarQixToolkit.Tests
     [TestFixture]
     public class FooBarQixToolkitTests
     {
-
         [TestCase("1Foo", "")]
         [TestCase("FooFoo", "")]
         [TestCase("QixFoo", "")]
         [TestCase("abc123", "")]
-        public void should_return_empty_string_when_input_is_not_number(string strNumber, string strExpected)
+        public void should_return_empty_string_when_input_is_not_number(string number, string expected)
         {
-            var computed = new FooBarQix().Compute(strNumber);
-            Assert.AreEqual(computed, strExpected);
+            var computed = new FooBarQix().Compute(number);
+            Assert.That(computed, Is.EqualTo(expected));
         }
         [TestCase("1", "1")]
         [TestCase("2", "2")]
         [TestCase("8", "8")]
-        public void should_return_number_when_input_is_regular_number(string strNumber, string strExpected)
+        public void should_return_number_when_input_is_regular_number(string number, string expected)
         {
-            var computed = new FooBarQix().Compute(strNumber);
-            Assert.AreEqual(computed, strExpected);
+            var computed = new FooBarQix().Compute(number);
+            Assert.That(computed, Is.EqualTo(expected));
+        }    
+        [TestCase("9", "Foo")]
+        [TestCase("96", "Foo")]
+        [TestCase("24", "Foo")]
+        public void should_return_foo_when_input_is_divisible_by_3(string number, string expected)
+        {
+            var computed = new FooBarQix().Compute(number);
+            Assert.That(computed, Is.EqualTo(expected));
+        }
+        [TestCase("13", "Foo")]
+        [TestCase("32", "Foo")]
+        public void should_return_foo_when_input_contains_3(string number, string expected)
+        {
+            var computed = new FooBarQix().Compute(number);
+            Assert.That(computed, Is.EqualTo(expected));
         }
         [TestCase("3", "FooFoo")]
-        public void should_return_foo_when_input_is_divisible_by_3_or_contains_3(string strNumber, string strExpected)
+        [TestCase("33", "FooFooFoo")]
+        public void should_return_foo_when_input_contains_3_and_is_divisisble_by_3(string number, string expected)
         {
-            var computed = new FooBarQix().Compute(strNumber);
-            Assert.AreEqual(computed, strExpected);
+            var computed = new FooBarQix().Compute(number);
+            Assert.That(computed, Is.EqualTo(expected));
         }
         [TestCase("5", "BarBar")]
-        public void should_return_foo_when_input_is_divisible_by_5_or_contains_5(string strNumber, string strExpected)
+        [TestCase("25", "BarBar")]
+        public void should_return_Bar_when_input_is_divisible_by_5_and_contains_5(string number, string expected)
         {
-            var computed = new FooBarQix().Compute(strNumber);
-            Assert.AreEqual(computed, strExpected);
+            var computed = new FooBarQix().Compute(number);
+            Assert.That(computed, Is.EqualTo(expected));
         }
-
+        [TestCase("52", "Bar")]
+        public void should_return_Bar_when_input_contains_5(string number, string expected)
+        {
+            var computed = new FooBarQix().Compute(number);
+            Assert.That(computed, Is.EqualTo(expected));
+        }
         [TestCase("7", "QixQix")]
-        public void should_return_foo_when_input_is_divisible_by_7_or_contains_7(string strNumber, string strExpected)
+        [TestCase("77", "QixQixQix")]
+        public void should_return_Qix_when_input_is_divisible_by_7_and_contains_7(string number, string expected)
         {
-            var computed = new FooBarQix().Compute(strNumber);
-            Assert.AreEqual(computed, strExpected);
+            var computed = new FooBarQix().Compute(number);
+            Assert.That(computed, Is.EqualTo(expected));
         }
-
+        [TestCase("28", "Qix")]
+        public void should_return_Qix_when_input_is_divisible_by_7(string number, string expected)
+        {
+            var computed = new FooBarQix().Compute(number);
+            Assert.That(computed, Is.EqualTo(expected));
+        }
+        [TestCase("17", "Qix")]
+        [TestCase("47", "Qix")]
+        public void should_return_foo_when_input_contains_7(string number, string expected)
+        {
+            var computed = new FooBarQix().Compute(number);
+            Assert.That(computed, Is.EqualTo(expected));            
+        }
         [TestCase("53", "BarFoo")]
         [TestCase("21", "FooQix")]
         [TestCase("51", "FooBar")]
-        [TestCase("15", "FooBarBar")]
-        [TestCase("33", "FooFooFoo")]
-        [TestCase("13", "Foo")]
+        [TestCase("15", "FooBarBar")]     
         [TestCase("101", "1*1")]
         [TestCase("303", "FooFoo*Foo")]
         [TestCase("105", "FooBarQix*Bar")]
         [TestCase("10101", "FooQix**")]
-        public void should_return_value_using_foo_bar_qix_rules(string strNumber, string strExpected)
+        public void should_return_value_using_foo_bar_qix_rules(string number, string expected)
         {
-            var computed = new FooBarQix().Compute(strNumber);
-            Assert.AreEqual(computed, strExpected);
+            var computed = new FooBarQix().Compute(number);
+            Assert.That(computed, Is.EqualTo(expected));
+            // Assert.AreEqual(computed, strExpected);
         }
     }
 }
